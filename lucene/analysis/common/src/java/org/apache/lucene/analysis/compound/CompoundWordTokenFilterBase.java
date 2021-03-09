a973d2bec612758a57e5c4c5a226529ca784485b
Pawel Kowalski <pawel.kowalski@orbit7.ch>
2021-03-09 21:11:20 +0100

    implement preserveOriginal=false for the DictDecompounder

--- lucene/analysis/common/src/java/org/apache/lucene/analysis/compound/CompoundWordTokenFilterBase.java
+++ lucene/analysis/common/src/java/org/apache/lucene/analysis/compound/CompoundWordTokenFilterBase.java
@@ -89,13 +89,7 @@ public abstract class CompoundWordTokenFilterBase extends TokenFilter {
   @Override
   public final boolean incrementToken() throws IOException {
     if (!tokens.isEmpty()) {
-      assert current != null;
-      CompoundToken token = tokens.removeFirst();
-      restoreState(current); // keep all other attributes untouched
-      termAtt.setEmpty().append(token.txt);
-      offsetAtt.setOffset(token.startOffset, token.endOffset);
-      posIncAtt.setPositionIncrement(1);
-      return true;
+      return this.processSubtokens();
     }
 
     current = null; // not really needed, but for safety
@@ -103,8 +97,12 @@ public abstract class CompoundWordTokenFilterBase extends TokenFilter {
       // Only words longer than minWordSize get processed
       if (termAtt.length() >= this.minWordSize) {
         decompose();
-        // only capture the state if we really need it for producing new tokens
+
+        // provided that we have sub-tokens, we don't want to write the original token into the output
         if (!tokens.isEmpty()) {
+          return this.processSubtokens();
+        } else {
+          // only capture the state if we really need it for producing new tokens
           current = captureState();
         }
       }
@@ -113,6 +111,19 @@ public abstract class CompoundWordTokenFilterBase extends TokenFilter {
     } else {
       return false;
     }
+  }
+
+  private boolean processSubtokens() {
+    if (!tokens.isEmpty()) {
+      assert current != null;
+      CompoundToken token = tokens.removeFirst();
+      restoreState(current); // keep all other attributes untouched
+      termAtt.setEmpty().append(token.txt);
+      offsetAtt.setOffset(token.startOffset, token.endOffset);
+      posIncAtt.setPositionIncrement(1);
+      return true;
+    }
+    return false;
   }
 
   /** Decomposes the current {@link #termAtt} and places {@link CompoundToken} instances in the {@link #tokens} list.
